a74d396e485fbc10e890ce5fb5a5a34f45660c43
Elijan5 <48808508+Elijan5@users.noreply.github.com>
2019-04-18 15:05:33 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var xpos =0
+
+function setup() {
+  // put setup code here
+   createCanvas (700, 400);
+    
+}
+
+function draw() {
+    background ("#cbf442");
+    fill ("#FF0000");
+    ellipse (xPos, height/2, 100, 100);
+    
+}
